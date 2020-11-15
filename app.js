4f7bda9eceec910a2fe80e182a944e90143f74e5
Kevin Forhan <kevin4forhan@gmail.com>
2020-11-14 18:44:33 -0600

    feat: added handle bars

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
  const express = require("express")
- 
+ const path = require('path')
  const data = require('./MOCK_DATA.json')
 
  const app = express()
 
+ app.set('views', path.join(__dirname, 'views'));  
+ app.set('view engine', 'hbs'); 
+ app.set(express.static('public')); 
  app.use('/random-country', (req, res) => {
-    const { country_name } = data[Math.round(MAth.random() * data.length)] 
+    const { country_name } = data[Math.round(Math.random() * data.length)]; 
     return res.json({ country_name })
  });  
 
 
- app.listen(8080, () => console.log('make sure its listening... '))
+ app.use('/', (req, res) => {
+     return res.render('index')
+ })
+
+ app.listen(8080, () => console.log('are you not entertained... '))
+ 
